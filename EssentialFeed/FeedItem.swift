df84611a38cbaec0162355393e9114c8a14e3f51
Ambrose <ambymbayi45@gmail.com>
2023-10-24 09:58:32 +0300

    create the FeedItem Model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Ambrose Mbayi on 24/10/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
