49ccd8333ef177ed684cec5efdaa5e6943bade04
Ambrose <ambymbayi45@gmail.com>
2023-10-25 11:15:20 +0300

    Rename RemoteFeedLoaderTests

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import EssentialFeed
 
-class RemoteFeedLoaderTesst: XCTestCase {
+class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
