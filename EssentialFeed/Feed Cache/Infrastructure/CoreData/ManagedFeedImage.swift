cf0610a3642182a9f981232cc6a879a3b69305a6
Ambrose <ambymbayi45@gmail.com>
2024-01-12 13:06:36 +0300

    Separate CoreData managed model classes data from helpers with extensions

--- EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -16,6 +16,9 @@ internal class ManagedFeedImage: NSManagedObject {
     @NSManaged var url: URL
     @NSManaged var cache: ManagedCache
     
+}
+ 
+extension ManagedFeedImage {
     internal static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
         return NSOrderedSet(array: localFeed.map { local in
             let managed = ManagedFeedImage(context: context)
