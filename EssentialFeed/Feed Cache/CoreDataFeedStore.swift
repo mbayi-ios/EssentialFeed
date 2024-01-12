8237650c4d44e4193902e352575cd23e1289e617
Ambrose <ambymbayi45@gmail.com>
2024-01-12 11:20:52 +0300

    Update CoreDataFeedStore.swift

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -13,7 +13,7 @@ public final class CoreDataFeedStore: FeedStore {
     private let context: NSManagedObjectContext
     
     public init(storeURL: URL, bundle: Bundle = .main) throws {
-        container = try NSPersistentContainer.load(modelName: "FeedStore", in: bundle)
+        container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: bundle)
         context = container.newBackgroundContext()
     }
     
